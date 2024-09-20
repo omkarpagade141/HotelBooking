7f713101d5f104fda82afcb28bccd41a5e123122
devgaurav4912 <pawargaurav49122@gmail.com>
2024-09-20 11:34:01 +0530

    user entity and jwt dto added

---
+++ Backend/HotelBooking/src/main/java/com/HotelBooking/HotelBooking/Entity/User.java
@@ -0,0 +1,83 @@
+package com.HotelBooking.HotelBooking.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
+
+    private String userName;
+
+    private String email;
+
+    private  String password;
+
+    private String roles;
+
+    public User(){
+        super();
+    }
+
+    public User(int user_id, String userName, String email, String password, String roles) {
+        this.user_id = user_id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_id=" + user_id +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", roles='" + roles + '\'' +
+                '}';
+    }
+}
